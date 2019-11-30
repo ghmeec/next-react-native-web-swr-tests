419501d512f5eaaadf1369f5ac68ca1dc87c2484
ghmeec <georgemillanzi1234@gmai.com>
2019-11-30 19:31:26 +0300

    integrated Nextjs. SWR and React Native Web

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ['next/babel'],
-  plugins: [['react-native-web', { commonjs: true }]],
+  plugins: [['react-native-web', { commonjs: true }],
+  // ["@babel/plugin-proposal-class-properties", { "loose": true }]
+  // ['module:react-native-responsive-layout', { commonjs: true }],
+  // ["@babel/plugin-transform-arrow-functions",{ commonjs: true }]
+
+],
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,85 @@
 import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Button,  ActivityIndicator, } from 'react-native'
+// import {Grid,Block,Section} from 'react-native-responsive-layout'
+import useSWR, { SWRConfig } from "swr";
+
+async function fetchTodos(...args) {
+  const res = await fetch(...args);
+  return await res.json();
+}
+
 
 export default function App(props) {
+  const [number,setNumber]=React.useState(0)
+  const { data, error } = useSWR(
+    //  using mongodb stictch webbook
+    //  for real time database update in mongodb atlas
+    //  now works in real time
+
+    "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/shutta-jrspv/service/fetchUsers/incoming_webhook/todoHook",
+    fetchTodos
+  );
+
+  if (error) return (
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text accessibilityRole="header" aria-level="2" style={styles.text}>
+          Lost internet connection
+        </Text>
+      </View>
+    </View>
+
+  );
+  if (!data) return (
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        {/* <Text accessibilityRole="header" aria-level="2" style={styles.text}>
+          Loading...
+        </Text> */}
+        <ActivityIndicator size="large" color="#0000ff" animating="true" />
+      </View>
+   </View>
+  );
+
   return (
     <View style={styles.container}>
       <Text accessibilityRole="header" style={styles.text}>
         React Native for Web & Next.js
       </Text>
 
+      <View style={styles.textContainer}>
+        <Text accessibilityRole="header" style={styles.text}>
+            Loaded Data
+        </Text>
+        {data.map((el,index)=>{
+          return(
+            <Text accessibilityRole="header" aria-level="3" style={styles.text}>
+              {el.title}
+           </Text>
+          )
+        })
+      }
+
+
+      </View>
+
       <Text style={styles.link} accessibilityRole="link" href={`/alternate`}>
         A universal link
       </Text>
-
+      
       <View style={styles.textContainer}>
         <Text accessibilityRole="header" aria-level="2" style={styles.text}>
-          Subheader
+          {number}
         </Text>
+        <Button
+          title="Add number"
+          onPress={()=>{
+            setNumber(number+1)
+          }}
+        >Add umber</Button>
       </View>
+
+
     </View>
   )
 }
