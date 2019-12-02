828ed1a97f8fe88c2d8ae2618dfafdf948ddc058
ghmeec <georgemillanzi1234@gmail.com>
2019-12-02 16:56:29 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,10 @@ module.exports = {
   plugins: [
   ['react-native-web', { commonjs: true }],
   // ["transform-class-properties", { commonjs: true }],
-  ["@babel/plugin-proposal-class-properties", { "loose": true }]
-  ['module:react-native-elements', { commonjs: true }],
-  ['module:react-native-vector-icons',{ commonjs: true }],
-  ['module:react-native-ratings',{ commonjs: true }]
+  // ["@babel/plugin-proposal-class-properties", { "loose": true }]
+  // ['module:react-native-elements', { commonjs: true }],
+  // ['module:react-native-vector-icons',{ commonjs: true }],
+  // ['module:react-native-ratings',{ commonjs: true }]
 
 ],
 }
